529373e4e5dbe78f708840f289da2c04c7f38c8f
prime <105910256+PrimeTDMomega@users.noreply.github.com>
2023-01-03 12:28:23 +0530

    added base

--- main.java
+++ main.java
@@ -1,1 +1,35 @@
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 
+public class MyMinecraftBot {
+  public static void main(String[] args) {
+    // Connect to the server and join a game
+    Minecraft mc = Minecraft.getMinecraft();
+    
+    // Continuously check for new chat messages
+    while (true) {
+      // Check if a chat message was received
+      if (mc.ingameGUI.getChatGUI().getChatOpen()) {
+        // Get the latest chat message
+        String chatMessage = mc.ingameGUI.getChatGUI().getChatMessage();
+        
+        // Check if the chat message starts with "!coords"
+        if (chatMessage.startsWith("!coords")) {
+          // Get the bot's current coordinates
+          int x = (int) mc.thePlayer.posX;
+          int y = (int) mc.thePlayer.posY;
+          int z = (int) mc.thePlayer.posZ;
+          
+          // Send a message back to the chat with the coordinates
+          String coordMessage = "My coordinates are: " + x + ", " + y + ", " + z;
+          mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + coordMessage));
+        }
+      }
+      
+      // Sleep for a short time before checking for new chat messages again
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        // Do nothing
+      }
